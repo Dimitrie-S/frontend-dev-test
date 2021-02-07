fec47731639b82e46e04664141b9e44c5efc19ad
Dimitrie Sandu <dimitrie_s@outlook.com>
2021-02-07 19:36:55 +0200

    data fetch

--- DigitalSand/BlackBullion/frontend-dev-test/src/App.js
+++ DigitalSand/BlackBullion/frontend-dev-test/src/App.js
@@ -1,12 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Card from "./components/Card";
 
- class App extends Component {
+class App extends Component {
+  state = {
+    cards: [],
+    isLoaded: false,
+  };
+
+  componentDidMount = () => {
+    fetch("http://www.blackbullion.com/_dev/api/lessons")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            cards: result.data,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+          console.log(error);
+        }
+      );
+  };
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    var isLoaded = this.state.isLoaded;
+    var cards = this.state.cards;
+    if (isLoaded) {
+      return (
+        <div>
+          {cards.map((item, index) => (
+            <Card
+              key={index}
+              title={item.title}
+              image={item.image}
+              description={item.description}
+              url={item.url}
+              duration={item.duration}
+            />
+          ))}
+        </div>
+      );
+    } else {
+      return <div style={{ textAlign: "center" }}>Loading...</div>;
+    }
   }
 }
-export default App
+export default App;
---
+++ DigitalSand/BlackBullion/frontend-dev-test/src/components/Card.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Card(props) {
+  var image = {
+    backgroundImage: "url(" + props.image + ")",
+    backgroundSize: "100%",
+  };
+  // Assuming that the time is in seconds, to display it in minutes:
+  var time = parseInt(props.duration / 60);
+
+  // In order not to display too much text:
+  var description = props.description.slice(0, 199);
+
+  return (
+    <div className="card">
+      <div className="card-image" style={image}>
+        <div className="card-time">{time} min</div>
+      </div>
+      <h1 className="card-title">{props.title}</h1>
+      <p className="card-description">
+        {description}
+        {props.description.length > 199 && <span>...</span>}
+      </p>
+
+      <div className="card-go">
+        <a href={props.url} className="card-link">
+          Start pathway &#8594;
+        </a>
+      </div>
+    </div>
+  );
+}
+Card.propTypes = {
+  duration: PropTypes.number,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  description: PropTypes.string,
+  url: PropTypes.string.isRequired,
+};
+
+export default Card;
